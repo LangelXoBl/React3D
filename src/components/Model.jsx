59482708541b443e4fbbe8c9dc1c2b9c99352963
langelX <langelxobl@gmail.com>
2023-03-27 01:25:54 -0500

    loader asses with url

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -3,7 +3,7 @@ import { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 //* importa las funcionalidades
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 const Model = () => {
   const mountRef = useRef(null);
 
@@ -35,19 +35,90 @@ const Model = () => {
     controls.enableDamping = true;
 
     //# se crea el objeto y se añade a la escena
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xab0097 });
+    /* const geometry = new THREE.BoxGeometry(1, 1, 1);
+    //* Este material no refleja luz
+    // const material = new THREE.MeshBasicMaterial({ color: 0xab0097 });
+    //* pero este si lo refleja
+    const material = new THREE.MeshPhongMaterial({ color: 0xab0097 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     //# le decimos a la camara que mire hacia el cubo
-    camera.lookAt(cube.position);
+    camera.lookAt(cube.position); */
+
+    //# Load model
+    const GLTF = new GLTFLoader();
+    GLTF.load(
+      'http://localhost:3000/api/v1/public/Bee2.glb',
+      (gltf) => {
+        console.log('cargado');
+        console.log(gltf.scene);
+        // gltf.scene.traverse((object) => {
+        //   // if (object.isMesh) {
+        //   //   object.material.map =
+        //   //     object.material.map || object.material.emissiveMap;
+        //   //   object.material.normalMap =
+        //   //     object.material.normalMap || object.material.map;
+        //   //   object.material.normalScale.set(1, -1);
+        //   // }
+        //   if (object.isMesh) {
+        //     object.material = new THREE.MeshBasicMaterial({ color: 0xab0097 });
+        //   }
+        // });
+        scene.add(gltf.scene);
+        camera.lookAt(gltf.scene.position);
+      },
+      () => {
+        console.log('cargando');
+      },
+      () => {
+        console.log('error');
+      }
+    );
+
+    //# se debe crear una luz ambiental para darle el color deseado
+    //* ayuda a darle el color natural
+    const ambientalLight = new THREE.AmbientLight(0xffffff, 5);
+    scene.add(ambientalLight);
+
+    //# se crea el punto de luz para la escena, pero le cambia el color al objeto
+    //* ayuda a darle brillo
+    const pointLight = new THREE.PointLight(0xffffff, 3);
+    pointLight.position.set(8, 8, 8);
+    scene.add(pointLight);
+
+    //# sirve para optimizar de manera sencilla las animaciones
+    const clock = new THREE.Clock();
 
     //# funcion que controla el movimiento
     const animate = () => {
+      //# esto optimiza la animacion
+      const elapsedTime = clock.getElapsedTime();
+
+      //# hace que se rote de manera automatica
+      /*cube.rotation.z = elapsedTime;
+      cube.rotation.x = elapsedTime;
+      cube.position.y = Math.sin(elapsedTime);*/
+      //# esto hace que se detenga suavemente
       controls.update();
+      //# Renderiza el nuevo frame y despues se llama a si mismo para seguir renderizando
+      //# esto tambien hace que se pueda manipular
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
     };
+
+    //# se saco del adEventListener para remover el evento cuando el componente sea desmontado
+    //* esto mejora el rendimiento de la web
+    const resize = () => {
+      const updateWidth = currentRef.clientWidth;
+      const updateHeight = currentRef.clientHeight;
+      renderer.setSize(updateWidth, updateHeight);
+      //# esto es para que no se deforme el objeto con el resize
+      camera.aspect = updateWidth / updateHeight;
+      camera.updateProjectionMatrix();
+    };
+
+    //# funcion para hacer el resize de la escena deacuerdo al cambio de la pantalla
+    window.addEventListener('resize', resize);
 
     //# se llama la funcion de control y como renderiza la scena ya no es necesario tener el otro
     animate();
@@ -55,7 +126,9 @@ const Model = () => {
     //# se renderiza la scena
     // renderer.render(scene, camera);
     return () => {
+      //# aqui se remueven los eventos
       currentRef.removeChild(renderer.domElement);
+      window.removeEventListener('resize', resize);
     };
   }, []);
   return (
